3e13aab3a6f416c8c8617c61fdf6f1773f391ac5
davesantos <info@davesantos.com>
2019-10-10 02:36:23 -0700

    Add sass

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "./layout.sass"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -36,9 +36,7 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © {new Date().getFullYear()} {data.site.siteMetadata.title}
         </footer>
       </div>
     </>
