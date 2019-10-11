f30de4e01f90edfe1c44334aa6e9ae914382e208
davesantos <info@davesantos.com>
2019-10-11 00:17:41 -0700

    Clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,16 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
+import Section from "../components/section"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Link to="/about/">About</Link>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <Section>Hello</Section>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
