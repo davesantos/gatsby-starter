6649f53f60d6f5ed4964ff497e9f8245ed78c769
davesantos <info@davesantos.com>
2019-10-11 00:17:57 -0700

    Add section

---
+++ src/components/section.js
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types"
+import React from "react"
+import styled from "styled-components"
+
+const TheSection = styled.section`
+
+  width: 100%;
+
+`
+
+const Section = ({children}) => (
+  <TheSection>
+    <article>
+      <h2>This is section!</h2>
+      {children}
+    </article>
+  </TheSection>
+)
+
+Section.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Section.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Section
