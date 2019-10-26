ab1db7a79372aa31668bacba3236f8532ea3d8a7
davesantos <info@davesantos.com>
2019-10-26 15:17:48 -0700

    Add margins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from "react"
 
-import { Layout, SEO, Section, Image } from "src/components"
+import { Layout, SEO, Project, Section, Image } from "src/components"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Section>Hello</Section>
+    <Section background="#ff0000">
+      <h1>This is section</h1>
+    </Section>
+    <Project />
     <Image />
   </Layout>
 )
