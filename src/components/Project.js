1820d22176b6cfe603d9138d1d1c555832b68dfe
davesantos <info@davesantos.com>
2019-10-26 15:16:04 -0700

    Add project

---
+++ src/components/Project.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components"
+
+import { Section, Image } from "src/components"
+
+const Project = () => (
+
+  <Section>
+    This is project!
+  </Section>
+
+)
+
+export default Project
