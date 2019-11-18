e26ddc38a760cb2306bec0f4941ee4dc4798f1a3
davesantos <info@davesantos.com>
2019-11-17 17:17:17 -0800

    Add Breakpoint

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,17 +10,19 @@ import {
 } from "src/components"
 
 
-const OneCol = styled.div`
+const Item = styled.div`
 
-  grid-column: span 3;
+  grid-column: 1 / -1;
 
+  @media only screen and (min-width: 768px) {
+    grid-column: ${props => props.position || "2 / span 4" };
+  }
+  @media only screen and (min-width: 1030px)
+  @media only screen and (min-width: 1240px)
 `
 
 const Cover = styled.section`
-
   padding: 5em
-
-
 `
 
 const Desc = styled.p`
@@ -33,25 +35,41 @@ const IndexPage = () => (
 
     <Cover>Hello bio.</Cover>
 
-    <Grid>
-    <OneCol>
-    <QueryImage name="we_render_1" />
-    </OneCol>
-    <OneCol>
-    <QueryImage name="we_render_1" />
-    </OneCol>
-    </Grid>
+    <Project tag="print">
+      <Grid>
 
-    <Desc>Album artwork for Tomorrow's Tulips pressed and printed on Burger Records label. I designed the entire LP sleeve and inside slips with accompanying photography and painting by Alex Knost.</Desc>
+        <Item position="1 /span 3">
+          <QueryImage name="we_render_1" />
+        </Item>
+
+        <Item position="span 3">
+          <QueryImage name="we_render_1" />
+        </Item>
+
+        <Item>
+          <Desc>Album artwork for Tomorrow's Tulips pressed and printed on Burger Records label. I designed the entire LP sleeve and inside slips with accompanying photography and painting by Alex Knost.</Desc>
+        </Item>
+
+      </Grid>
+      
+    </Project>
 
     <Project tag="website">
+
+    <Grid>
+    <Item>
+    <QueryImage name="web_int_1" />
+    </Item>
     Website for Dominic Santos. Designed with a mobile-centric experience for Instagram majority traffic.
+    </Grid>
     </Project>
 
     <Project tag="website">
     Website for Diagonal Press built on the Shopify platform.
     </Project>
+
     <Project tag="website">
+
     <QueryImage name="web_int_1" />
 
     <p>UI refresh for Intero's stationery shop</p>
