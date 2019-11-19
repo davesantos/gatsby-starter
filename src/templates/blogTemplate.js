58e186f564eb3c909ed77e52713d1679a1e9be7f
davesantos <info@davesantos.com>
2019-11-19 13:23:57 -0800

    Add working markdown query

---
+++ src/templates/blogTemplate.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { Layout } from "components"
+
+export default function Template({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark
+  return (
+    <Layout>
+      <h1>{frontmatter.title}</h1>
+      <span>{frontmatter.date}</span>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`
