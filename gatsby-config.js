16232cdaeadc664d7132093f527f2385fab67633
davesantos <info@davesantos.com>
2019-11-19 14:09:54 -0800

    Move posts to pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: 'posts',
-        path: `${__dirname}/src/posts`,
+        path: `${__dirname}/src/pages`,
 			},
 		},
     `gatsby-transformer-remark`,
