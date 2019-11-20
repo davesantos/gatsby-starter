65d06cb6d5bf53208481ce076c0eae2a6305b1d6
davesantos <info@davesantos.com>
2019-11-20 11:20:10 -0800

    Add PostLink markdown list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react"
+import { graphql } from "gatsby"
 import styled from "styled-components"
-
 import {
   Layout,
   Project,
   QueryImage,
   Item,
   SEO,
+  PostLink,
 } from "src/components"
 
 const Cover = styled.section`
@@ -17,11 +18,23 @@ const Desc = styled.p`
   color: #bbb;
 `
 
-const IndexPage = ({data}) => {
+const IndexPage = ({
+
+    data: {
+      allMarkdownRemark: { edges },
+    },
+
+}) => {
+
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
 
   return (
   <Layout>
     <SEO title="Home" />
+
+    {Posts}
 
     <Cover>I am a Los Angeles based designer specializing in graphic design, illustration, brand identity, web design and development.</Cover>
 
@@ -35,8 +48,7 @@ const IndexPage = ({data}) => {
       </Item>
 
       <Item>
-        <Desc>Album artwork for Tomorrow's Tulips pressed and printed on Burger Records label. I designed the entire LP sleeve and inside slips with accompanying photography and painting by Alex Knost.</Desc>
-
+        <Desc>Album artwork for Tomorrow's Tulips</Desc>
         <a href="/experimental-jelly">More</a>
       </Item>
     </Project>
@@ -75,6 +87,25 @@ const IndexPage = ({data}) => {
 
 
     </Layout>
+
 )}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
