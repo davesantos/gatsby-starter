1ad48f8f689f4b32a1b309cfff99df12a5f7a461
davesantos <info@davesantos.com>
2019-11-21 19:55:24 -0800

    Move Item to primitives

--- src/primitives/index.js
+++ src/primitives/index.js
@@ -1,3 +1,4 @@
 export { default as Image } from './Image'
 export { default as QueryImage } from './QueryImage'
 export { default as Heading } from './Heading'
+export { default as Item } from './Item'
